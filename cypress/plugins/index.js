8d7fb13223f8df17184a592de03dcb785a108b6c
lteixei <53909935+lteixei@users.noreply.github.com>
2025-07-28 15:32:40 -0300

    Delete cypress directory

--- cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (on, config) => {
-  
-}
-
-const cucumber = require('cypress-cucumber-preprocessor').default
-module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
-}
